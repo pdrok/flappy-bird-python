facce39f73fa3b752029e6cc03d623a4171729ed
Pedro Lopez <plopez@poweranalytics.com>
2022-02-07 19:32:59 -0300

    collisions with rects

--- main.py
+++ main.py
@@ -30,6 +30,19 @@ def draw_pipes(pipes):
             screen.blit(flip_pipe, pipe)
 
 
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            print("collision")
+            return False
+
+    if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        print("collision")
+        return False
+
+    return True
+
+
 pygame.init()
 screen = pygame.display.set_mode((567, 1024))
 clock = pygame.time.Clock()
@@ -37,6 +50,7 @@ clock = pygame.time.Clock()
 # Game Variables
 gravity = 0.25
 bird_movement = 0
+game_active = True
 
 bg_surface = pygame.image.load("assets/background-day.png").convert()
 bg_surface = pygame.transform.scale2x(bg_surface)
@@ -64,21 +78,30 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and game_active:
                 bird_movement = 0
                 bird_movement -= 12
+            if event.key == pygame.K_SPACE and game_active == False:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100, 512)
+                bird_movement = 0
+
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
     screen.blit(bg_surface, (0, 0))
-    # Bird
-    bird_movement += gravity
-    bird_rect.centery += bird_movement
-    screen.blit(bird_surface, bird_rect)
+    if game_active:
+        # Bird
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        screen.blit(bird_surface, bird_rect)
+        game_active = check_collision(pipe_list)
 
-    # Pipes
-    pipe_list = move_pipes(pipe_list)
-    draw_pipes(pipe_list)
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
+
     # Fllor
     floor_x_pos -= 1
     draw_floor()
